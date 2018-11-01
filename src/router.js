4552666d8b4ada06b5d2a46a26a983972cfd8a67
Fer Gonz <fer@boicott.eu>
2018-11-01 12:10:45 +0100

    First router changes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Test from './views/Test.vue';
+import About from './views/About.vue';
 
 Vue.use(Router);
 
@@ -17,12 +19,12 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: About,
     },
     {
-       path: '/test',
-	name: test,
-	component: () => import('./views/Test.vue'),
-   },
+      path: '/test',
+      name: 'test',
+      component: Test,
+    },
   ],
 });
---
+++ src/views/Test.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="Test">
+        <h1>Hoooooola Router</h1>
+    </div>
+</template>
